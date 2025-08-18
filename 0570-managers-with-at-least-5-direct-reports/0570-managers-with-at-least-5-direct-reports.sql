907dabd4cb6a64c611d1d59f86d72c3cbb479363
Kauan Buzone Roberti <152347696+Kauan-bzn@users.noreply.github.com>
2025-08-18 12:41:20 -0300

    Time: 517 ms (9.39%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,6 @@
 # Write your MySQL query statement below
-select Employee.name
-from Employee inner join (
-    select managerId, count(managerId) as contagem
-    from Employee
-    group by managerId
-    having contagem >=5
-    ) as Managers
-    on Employee.id = Managers.managerId 
+SELECT a.name FROM Employee a
+JOIN Employee b
+ON a.id = b.managerID 
+GROUP BY b.managerID
+HAVING COUNT(*) >= 5;
